13d551341f5ba5ef217846c7101c055666c72aff
Pedro Mattos <phmattos0@gmail.com>
2022-03-08 22:09:04 -0300

    Aula 3 listas e metodos de listas - map(),slice(),pop() objetos - let aluno = { aluno: "pedro" };

---
+++ aula03/listas.js
@@ -0,0 +1,38 @@
+let listaCarros = ["um","dois","tres","tres","tres","tres","tres","tres","tres","tres","tres"];
+
+console.log(listaCarros);
+for(i = 0; i < 10; i++){
+
+}
+while(contador<10){
+
+}
+
+do{
+
+}while(contador<10);
+
+
+
+class aluno{
+    constructor(nome,idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    imprimi(){
+        console.log(this.idade);
+    }
+}
+
+let lista = [
+    {
+        nome:"Pedro"
+    },
+    {
+        nome:"Algusto"
+    }
+]
+
+aluno = new aluno("pedro",27);
+console.log(aluno.nome);
+aluno.imprimi();
