7b1928d3fdbbebb6cb009d37fc1272338d47c9bf
Pedro Mattos <phmattos0@gmail.com>
2022-03-14 21:35:08 -0300

    new

--- aula01/fizzbuzz/solucao.js
+++ aula01/fizzbuzz/solucao.js
@@ -1,12 +1,11 @@
 function Resolve(number){
-    if(number<=100){
+    if(number<=100){                                                                                    
         if(number%3 == 0){
             console.log("fizz");
         }
-        
         if(number%5 == 0){
             console.log("buzz");
-        }
+        }                                                                                
         if(number%3 == 0 && number%5==0){
             console.log("fizz buzz");
         }
