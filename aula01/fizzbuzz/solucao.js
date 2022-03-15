4db1b7f331afd2af2adac526e799032006ec0ee9
Pedro Mattos <phmattos0@gmail.com>
2022-03-14 21:58:36 -0300

    final

--- aula01/fizzbuzz/solucao.js
+++ aula01/fizzbuzz/solucao.js
@@ -1,19 +1,19 @@
 function Resolve(number){
-    if(number<=100){                                                                                    
-        if(number%3 == 0){
-            console.log("fizz");
-        }
-        if(number%5 == 0){
-            console.log("buzz");
-        }                                                                                
+    if(number<=100){   
         if(number%3 == 0 && number%5==0){
             console.log("fizz buzz");
-        }
-        if(number%3 != 0 && number%5 !=0){
-            console.log(number);
-        }
-        
-        
+        }else{
+            if(number%3 == 0){
+                console.log("fizz")
+            }
+            if(number%5 == 0){
+                console.log("buzz");
+            }
+            if(number%3 != 0 && number%5 !=0){
+                console.log(number);
+            }
+            
+        }    
             
         Resolve(number+1);
     }
