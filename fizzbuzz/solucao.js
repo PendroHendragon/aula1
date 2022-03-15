21f8d6cc805b6c8d39eedc1276de71a4d61f49d1
Pedro Mattos <phmattos0@gmail.com>
2022-03-14 21:33:43 -0300

    fizzbuzz Challenge

---
+++ fizzbuzz/solucao.js
@@ -0,0 +1,24 @@
+function Resolve(number){
+    if(number<=100){
+        if(number%3 == 0){
+            console.log("fizz");
+        }
+        
+        if(number%5 == 0){
+            console.log("buzz");
+        }
+        if(number%3 == 0 && number%5==0){
+            console.log("fizz buzz");
+        }
+        if(number%3 != 0 && number%5 !=0){
+            console.log(number);
+        }
+        
+        
+            
+        Resolve(number+1);
+    }
+    
+
+}
+Resolve(1);
